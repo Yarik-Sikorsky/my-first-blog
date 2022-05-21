4a8fa78d391dcce3d109d24c8ab800dd02832f09
Your Name <you@example.com>
2022-05-21 22:57:11 +0300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
 ]
